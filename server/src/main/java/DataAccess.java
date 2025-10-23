148d69306a80cc2532d0c35b739d2d5bcf2670cd
Shania Ziegler <shaniaziegler@gmail.com>
2025-10-22 22:58:16 -0600

    just some comments

---
+++ server/src/main/java/DataAccess.java
@@ -0,0 +1,26 @@
+public class DataAccess {
+
+    /*
+    Define the contracts (method signatures) for all necessary CRUD operations, such as:
+
+clear()
+
+createUser(UserData user)
+
+getUser(String username)
+
+createAuth(AuthData auth)
+
+... (and methods for Games)
+
+Important: Ensure all methods specify throws DataAccessException.
+Create the class that actually implements the interface you just defined, using in-memory data structures like HashMap for storage.
+
+Location: server/src/main/java/dataaccess
+
+File: MemoryDataAccess.java
+
+Action: Implement all the methods from the DataAccess.java interface using Java collections to hold your UserData, GameData, and AuthData objects.
+
+     */
+}
