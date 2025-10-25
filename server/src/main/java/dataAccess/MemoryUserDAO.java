6d511dff12c0ea341bc060759dd866a085852bd8
Shania Ziegler <shaniaziegler@gmail.com>
2025-10-25 17:54:58 -0600

    My first attempt at memory dao this needs a lot of work it's pseudo code still sort of

--- server/src/main/java/dataAccess/MemoryUserDAO.java
+++ server/src/main/java/dataAccess/MemoryUserDAO.java
@@ -2,15 +2,27 @@ package dataAccess;
 
 import model.UserData;
 import java.util.HashMap;
-import java.util.Map.
+import java.util.Map;
 
 
 public class MemoryUserDAO implements UserDAO{
 
-    private final Map<String, UserData> users = ?;
+    private final Map<String, UserData> users = new HashMap<>();
+
+    /*
+   do users come from where my user data right i am not sure yet how to implement
+   look at the map to see whrre i can put in but need something like
+
+   user.get
+   user.put
+   ect
+     */
+
 
     @Override
     public void clear() throws DataAccessException{
+        HashMap.clear<>(UserData);
+
         //my hashmap method and empty hash map
     }
 
@@ -18,20 +30,30 @@ public class MemoryUserDAO implements UserDAO{
     public void createUser(UserData user) throws DataAccessException{
         //check if user is null or user name is null throw error for bad request if so
 
+        if(user == null){
+            throw new DataAccessException("Error Bad Request user object is null");
+        }
 
-    /*check if user exist in hash map
-    since has keeps a key do hashmap contains key username
-    if so throw new dataaccess exception with already taken :)
+        if(user.username() == null){
+            throw new DataAccessException("Username is missing or blank retry");
+        }
+
+        //stores user
+        user.put(key, value)
 
 
 
-        part 3 3rd method i can think of for now add user to hash map with hashmap key
-        then key will become user and value username
-     */
+    //check if user exist in hash map
+
+    if (users.containsKey(User)){
+        throw new DataAccessException("Error username already taken");
     }
 
+    if(users.containsKey  )
+
     @Override
-    public UserData getUser(String username) throws DataAccessException{
+    public UserData getUser(String username)
+        throws DataAccessException{
         /*
         look up username in hash map return user data return value or null
          */
