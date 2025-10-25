362823ef968a6763ce2188fcbca8289c23e8efb6
Shania Ziegler <shaniaziegler@gmail.com>
2025-10-24 18:28:16 -0600

    Added some  method signatures to my AuthDAO interface

--- server/src/main/java/dataAccess/AuthDAO.java
+++ server/src/main/java/dataAccess/AuthDAO.java
@@ -3,5 +3,8 @@ package dataAccess;
 import model.AuthData;
 
 public interface AuthDAO {
-
+    void clear() throws DataAccesssException;
+    void createAuth(AuthDatam, auth) throws DataAccesssException;;
+    AuthData getAuth(String authToken) throws DataAccesssException;;
+    void deleteAuth(String authToken) throws DataAccesssException;
 }
