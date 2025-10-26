a8f15ac9cd6853741b00eb78f817a81e377f3103
Shania Ziegler <shaniaziegler@gmail.com>
2025-10-26 14:02:33 -0600

    added login successs test

--- server/src/test/service/UserServiceTest.java
+++ server/src/test/service/UserServiceTest.java
@@ -34,27 +34,40 @@ public class UserServiceTest {
 
         //verify correct results
 
-        assertNotNull(result.authToken());
+        assertNotNull(result.authToken(),"Auth token needs to be generated");
 
-        assertEquals("zap",result.username());
+        assertEquals("zap",result.username(),"Username in result match request");
     }
 
     @Test
     @DisplayName("Register for duplicate username test")
 
     public void registerDuplicate() throws DataAccessException{
-        var req1 = new UserService.RegisterRequest("zap","password!","email12");userService.register(req1);
+        var req1 = new UserService.RegisterRequest("zap","password!","email12@mail.com");userService.register(req1);
+        assertDoesNotThrow(() -> userService.register(req1));
+        var req2 = new UserService.RegisterRequest("zap","pas123","ziggles@mail.com");
 
-        var req2 = new UserService.RegisterRequest("zap","pas123","ziggles");
 
-
-        //check if code throws excpected exception with () ->
+        //check if code throws expected exception with () ->
         var exception = assertThrows(DataAccessException.class,() -> userService.register(req2));
 
         assertTrue(exception.getMessage().contains("Username 'zap' already exists"));
     }
 
+    @Test
+    @DisplayName("Login Success")
+    public void loginSuccess() throws DataAccessException{
+        var registerReq = new UserService.RegisterRequest("emily","pass45","iceberg@mail.com");
+        assertDoesNotThrow(() -> userService.register(registerReq));
 
+        var loginReq = new UserService.LoginRequest("emily","pass45");
+        var result = assertDoesNotThrow(() -> userService.register(registerReq));
+
+
+        assertNotNull(result.authToken(),"Auth token must be gen on successful login.");
+        assertEquals("emily",result.username(), "Logged in username needs to be correct");
+
+    }
 
 
 }
