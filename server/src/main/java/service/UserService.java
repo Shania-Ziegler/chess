63b2adaa10b086e8c76b4dfbf8eabbd093b70b5d
Shania Ziegler <shaniaziegler@gmail.com>
2025-10-26 01:00:56 -0600

    adds the userservice class defines the method signatures gives  intiial input validation for login and logout methods

---
+++ server/src/main/java/service/UserService.java
@@ -0,0 +1,55 @@
+package service;
+
+import dataaccess.UserDAO;
+import model.UserData;
+import java.util.UUID;
+
+public class UserService(UserDAO userDAO, AuthDAO authDAO) {
+    this.userDAO =UserDAO;
+    this.authDAO =authDAO;
+
+
+//request and result record
+
+//register
+
+/*
+create user data
+saveuser data to database
+call userdao.createuser(user)
+
+create token using the uuid random to string
+
+create/store auth data
+
+return relust
+ */
+
+    public LoginResult login(LoginRequest userreq) throws DataAccessException {
+        if (userreq.username == null || userreq.password == null) {
+            throw new DataAccessException("Error: bad request");
+        }
+
+        /*
+        get user from database
+        veryfiy user exists
+        generate auth token
+        create + store auth data
+        return reuslt
+         */
+    }
+
+    public void logout(String authToken) throws DataAccessException {
+        if (authToken == null) {
+            throw new DataAccessException("Error: unauthorized");
+        }
+    }
+
+    public void clear() throws DataAccessException{
+
+    }
+
+}
+
+
+
