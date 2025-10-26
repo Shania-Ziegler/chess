cbc2e1fb523f8f7b66b455f1e2b64e24aaadb4a0
Shania Ziegler <shaniaziegler@gmail.com>
2025-10-25 22:44:00 -0600

    did method create game still in progress

--- server/src/main/java/dataaccess/MemoryGameDAO.java
+++ server/src/main/java/dataaccess/MemoryGameDAO.java
@@ -1,4 +1,30 @@
 package dataaccess;
 
-public class MemoryGameDAO {
+import model.GameData;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+
+public class MemoryGameDAO implements GameDAO {
+
+    //look up game via id number
+    private final Map<Integer, GameData> games = new HashMap<>();
+
+
+    //adding the increment keeping track of game
+    private int nextGameID = 1;
+
+
+    @Override
+    private int CreateGame(GameData game) throws DataAccessException{
+        if (game == null || game.gameName() == null){
+            throw new DataAccessException("Error: bad request");
+        }
+
+        int gameID = nextGameID;
+
+        //create the game data with the real id
+    }
+    return gameID;
+
 }
