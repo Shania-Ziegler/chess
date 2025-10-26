79928ff727e9d991eb8c7de1b10be6b592b84400
Shania Ziegler <shaniaziegler@gmail.com>
2025-10-26 15:11:37 -0600

    added fields in game service test

--- server/src/test/service/GameServiceTest.java
+++ server/src/test/service/GameServiceTest.java
@@ -1,4 +1,19 @@
 package service;
 
+import dataaccess.*;
+import service.*;
+import model.*;
+import org.junit.jupiter.api.*;
+
 public class GameServiceTest {
+    private UserDAO userDAO;
+    private AuthDAO authDAO;
+    private GameDAO gameDAO;
+    private UserService userService;
+    private GameService gameService;;
+}
+
+@BeforeEach
+public void setup(){
+
 }
