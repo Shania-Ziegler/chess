51c1327c6923afb8960d4cb45123d220ed433913
Shania Ziegler <shaniaziegler@gmail.com>
2025-10-26 02:11:19 -0600

    implement the rest of the records

--- server/src/main/java/service/GameService.java
+++ server/src/main/java/service/GameService.java
@@ -5,22 +5,32 @@ import dataaccess.GameDAO;
 import dataaccess.DataAccessException;
 import model.AuthData;
 import model.GameData;
-import model.JoinGameRequest;
+import chess.ChessGame;
+import java.util.Collection;
 
 public class GameService {
-
     private final GameDAO gameDAO;
     private final AuthDAO authDAO;
 
 public GameService(GameDAO gameDAO, AuthDAO authDAO){
-    this.gameDAO;
-    this.authDAO;
+    this.gameDAO = gameDAO;
+    this.authDAO = authDAO;
 }
     //create game list game
 
+    public record CreateGameRequest{String gameName){}
     public record JoinGameRequest(String playerColor, Integer gameID){}
+    public record ListGameResult(Collection<GameData> games){}}
+    public record CreateGameResult(String gameName){}
 
-    public record JoinGameResult{}
+    public CreateGameResult createGame(String authToken, CreateGameRequest req) throws DataAccessException{
+    validateAuth(authToken);
+
+    if(req.gameName()==null){
+        throw new DataAccessException("Error: bad request");
+    }
+
+    GameData game = new GameData(0)
 
 }
 
