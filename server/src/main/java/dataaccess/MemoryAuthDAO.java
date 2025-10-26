b3627dc13cc67578cebaa68a8be51fdae9c4d721
Shania Ziegler <shaniaziegler@gmail.com>
2025-10-25 22:08:44 -0600

    Added create auth implemented delete auth needs to be put and retrieve/get auth

--- server/src/main/java/dataaccess/MemoryAuthDAO.java
+++ server/src/main/java/dataaccess/MemoryAuthDAO.java
@@ -14,5 +14,26 @@ public class MemoryAuthDAO implements AuthDAO {
     }
 
 
+    //check if we have valid token + put auth data in the hash map using token as key value
+    @Override
+    public void createAuth(AuthData auth) throws DataAccessException{
+        if(auth == null || auth.authToken() == null){
+            throw new DataAccessException("Error Bad request");
+        }
+    }
+
+
+    //check to see if token valid and tells server if someone logged in?
+    @Override
+
+    //delete auth
+    @Override
+    public void deleteAuth(String authToken) throws DataAccessException{
+        if(authToken == null || authTokens.haskey(authToken)){
+            throw new DataAccessException("Error unauthorized");
+        }
+     authTokens.remove(authToken);
+    }
+
 
 }
