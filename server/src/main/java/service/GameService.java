166368d91879a13a1711416a7f90dbe6c1f8e9e4
Shania Ziegler <shaniaziegler@gmail.com>
2025-10-26 02:04:30 -0600

    added constructors to GameService and records

--- server/src/main/java/service/GameService.java
+++ server/src/main/java/service/GameService.java
@@ -1,6 +1,26 @@
 package service;
 
-
+import dataaccess.AuthDAO;
+import dataaccess.GameDAO;
+import dataaccess.DataAccessException;
+import model.AuthData;
+import model.GameData;
+import model.JoinGameRequest;
 
 public class GameService {
+
+    private final GameDAO gameDAO;
+    private final AuthDAO authDAO;
+
+public GameService(GameDAO gameDAO, AuthDAO authDAO){
+    this.gameDAO;
+    this.authDAO;
 }
+    //create game list game
+
+    public record JoinGameRequest(String playerColor, Integer gameID){}
+
+    public record JoinGameResult{}
+
+}
+
