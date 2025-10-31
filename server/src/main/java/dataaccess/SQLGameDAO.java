e69237f7ae7784156e57f87888276a0edf02e089
Shania Ziegler <shaniaziegler@gmail.com>
2025-10-30 22:46:16 -0600

    added methods to my SQL game interface

--- server/src/main/java/dataaccess/SQLGameDAO.java
+++ server/src/main/java/dataaccess/SQLGameDAO.java
@@ -3,5 +3,29 @@ package dataaccess;
 import model.GameData;
 import java.util.Collection;
 
-public class SQLGameDAO {
-}
+public class SQLGameDAO implements GameDAO {
+    @Override
+    public int createGame(String gameName) throws DataAccessException {
+        throw new DataAccessException("Not implemented yet");
+    }
+
+    @Override
+    public GameData getGame(int gameID) throws DataAccessException {
+        throw new DataAccessException("Not implemented yet");
+    }
+
+    @Override
+    public Collection<GameData> listGames() throws DataAccessException {
+        throw new DataAccessException("Not implemented yet");
+    }
+
+    @Override
+    public void updateGame(int gameID, GameData game) throws DataAccessException {
+        throw new DataAccessException("Not implemented yet");
+    }
+
+    @Override
+    public void clear() throws DataAccessException {
+        throw new DataAccessException("Not implemented yet");
+    }
+}git
