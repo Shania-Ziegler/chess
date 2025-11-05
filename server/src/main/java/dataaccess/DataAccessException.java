85e7f001ddcdf5c9c513f0014264789f581a685e
Shania Ziegler <shaniaziegler@gmail.com>
2025-11-04 22:22:47 -0700

    Phase 4: Fixed missing braces and test exception declaration

--- server/src/main/java/dataaccess/DataAccessException.java
+++ server/src/main/java/dataaccess/DataAccessException.java
@@ -13,10 +13,12 @@ public class DataAccessException extends Exception {
     }
 
     private static String formatMessage(String message) {
-        if (message == null) return "Error: unspecified database failure";
+        if (message == null){
+            return "Error: unspecified database failure";
+        }
         if (!message.toLowerCase().contains("error")) {
             return "Error: " + message;
         }
         return message;
-    }//add test
+    }
 }
