864c268c74f5c8619bdf52a94f8cd4e3d2a58250
Shania Ziegler <shaniaziegler@gmail.com>
2025-11-17 05:18:54 -0700

    added serverfacade test setup

---
+++ client/src/main/java/client/ServerFacadeTests.java
@@ -0,0 +1,27 @@
+package client;
+
+import org.junit.jupiter.api.*;
+import server.Server;
+
+public class ServerFacadeTests {
+    private static Server server;
+    private static ServerFacade facade;
+
+    @BeforeAll
+    public static void init() {
+        server = new Server();
+        var port = server.run(0);
+        System.out.println("Started test HTTP server on " + port);
+        facade = new ServerFacade(port);
+    }
+@AfterAll
+    static void stopServer(){
+        server.stop();
+}
+@BeforeEach
+    void clearDatabase() throws Exception{
+        facade.clearDatabase();
+}
+
+}
+
