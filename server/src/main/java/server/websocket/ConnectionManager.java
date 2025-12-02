a35a20175f4ba244fce92aff48b416a655f0317c
Shania Ziegler <shaniaziegler@gmail.com>
2025-12-01 20:20:26 -0700

    Have implemented some of public class connection manager adding connection/game ID

--- server/src/main/java/server/websocket/ConnectionManager.java
+++ server/src/main/java/server/websocket/ConnectionManager.java
@@ -1,6 +1,23 @@
 package java.server.websocket;
+import com.google.gson.Gson;
+import org.eclipe.jetty.websocket.api.Session;
+import websocket.messages.ServerMessage;
+
+
+import java.io.IOException;
+import java.util.*;
+import java.util.ConcurrentHashMap;
 
 public class ConnectionManager {
+
+private final ConcurrentHashMap<Integer, ArrayList<Connection>> connections = new ConcurrentHashMap<>();
+
+private final Gson gson = new Gson();
+
+public void add(Integer gameID,String authToken, Session session){
+    var conn = new Connection(authToken, Session);
+    connections.com(gameID k -> new ArrayList<>()).add(conn);
+}
 
     //tracks active websocket connections
 }
