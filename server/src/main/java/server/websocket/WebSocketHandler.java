02dad708b7d51645ca29dd0a2b7a106f042408d2
Shania Ziegler <shaniaziegler@gmail.com>
2025-12-05 20:31:13 -0700

    added websockethandler methods for clossing and opening connection

--- server/src/main/java/server/websocket/WebSocketHandler.java
+++ server/src/main/java/server/websocket/WebSocketHandler.java
@@ -1,5 +1,23 @@
 package java.server.websocket;
 
+import com.google.gson.Gson;
+
+//@Websocket
 public class WebSocketHandler {
+  private final ConnectionManager connections = new ConnectionManager();
+  private final Gson gson = new Gson();
+
+
     //handles the incoming websocket connections
+
+    @OnWebSocketConnect
+    public void onConnect(Session session) {
+        System.out.println("WebSocket connected: " + session);
+    }
+
+    @OnWebSocketClose
+    public void onClose(Session session, int statusCode, String reason) {
+        System.out.println("WebSocket closed: " + session);
+    }
+
 }
