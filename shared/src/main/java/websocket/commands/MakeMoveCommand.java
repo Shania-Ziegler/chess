000d2beb8b1926a1aaa33d7c976a8b2fcf6a0df4
Shania Ziegler <shaniaziegler@gmail.com>
2025-12-09 10:45:34 -0700

    added make move command method

--- shared/src/main/java/websocket/commands/MakeMoveCommand.java
+++ shared/src/main/java/websocket/commands/MakeMoveCommand.java
@@ -1,4 +1,17 @@
 package websocket.commands;
+import chess.ChessMove;
 
-public class MakeMoveCommand {
+public class MakeMoveCommand extends UserGameCommand {
+    private final ChessMove move;
+
+    public MakeMoveCommand(String authToken, Integer gameID, ChessMove move) {
+        super(authToken);
+        this.commandType = CommandType.MAKE_MOVE;
+        this.gameID = gameID;
+        this.move = move;
+    }
+
+    public ChessMove getMove() {
+        return move;
+    }
 }
