0d77429b94fed15da34d2d8987d5b6df6f12f4a3
Shania Ziegler <shaniaziegler@gmail.com>
2025-12-09 20:35:33 -0700

    Added methods to gameplayUI

--- client/src/main/java/ui/GameplayUI.java
+++ client/src/main/java/ui/GameplayUI.java
@@ -9,6 +9,8 @@ import java.io.IOException;
 import java.util.Collection;
 import java.util.Scanner;
 
+import static ui.BoardDrawer.drawBoard;
+
 public class GameplayUI implements NotificationHandler {
     private final WebSocketFacade ws;
     private ChessGame currentGame;
@@ -32,12 +34,29 @@ public class GameplayUI implements NotificationHandler {
 
         ws.connect(authToken, gameID);
     }
+
     @Override
-    public void notify(ServerMessage message){
-        switch (message.getServerMessageType()){
+    public void notify(ServerMessage message) {
+        switch (message.getServerMessageType()) {
             case LOAD_GAME -> handleLoadGame((LoadGameMessage) message);
             case ERROR -> handleError((ErrorMessage) message);
             case NOTIFICATION -> handleNotification((NotificationMessage) message);
         }
     }
+    private void handleLoadGame(LoadGameMessage msg){
+        this.currentGame = msg.getGame();
+        System.out.println();
+        drawBoard();
+        System.out.print("\n>>>");
+    }
+    private void handleError(ErrorMessage msg) {
+        System.out.println(EscapeSequences.SET_TEXT_COLOR_RED + "\n" + msg.getErrorMessage() + EscapeSequences.RESET_TEXT_COLOR);
+        System.out.print(">>> ");
+    }
+
+    private void handleNotification(NotificationMessage msg) {
+        System.out.println(EscapeSequences.SET_TEXT_COLOR_BLUE + "\n" + msg.getMessage() + EscapeSequences.RESET_TEXT_COLOR);
+        System.out.print(">>> ");
+    }
+
 }
